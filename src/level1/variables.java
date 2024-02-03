ad3f76faa326f16f64383e447f257e419a690449
AJ913 <adwinjustamanuel913@gmail.com>
2024-02-03 23:04:08 +0530

    LETS LEARN JAVA LETSSSS GOOOOO!!!!!!!!!!!

---
+++ src/level1/variables.java
@@ -0,0 +1,11 @@
+package level1;
+
+public class variables {
+    public static void main(String[] args) {
+        int my_number = 65; // my number declaration and equals 865 declaration
+        int your_name= 865;
+        System.out.println(your_name);
+        System.out.println(my_number);
+    }
+
+}
