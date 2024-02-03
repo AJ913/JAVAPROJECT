2d99f01c0150ca0a08ad8ea8a298f0ff4dc8ef97
AJ913 <adwinjustamanuel913@gmail.com>
2024-02-03 20:55:49 +0530

    LETS LEARN JAVA LETSSSS GOOOOO!!!!!!!!!!!

---
+++ src/challenge/pattern1.java
@@ -0,0 +1,10 @@
+package challenge;
+
+public class pattern1 {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+}
